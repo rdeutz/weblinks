c6c10ecc9d803e92ba023939d181efc494a4462d
Robert Deutz <rdeutz@googlemail.com>
2015-10-30 15:35:14 +0100

    wip

--- RoboFile.php
+++ RoboFile.php
@@ -188,7 +188,7 @@ class RoboFile extends \Robo\Tasks
 
 		// Copy cache to the testing folder
 		$this->_copyDir('tests/cache', $this->cmsPath);
-
+		$this->_exec('chown -R joomla ' . $this->cmsPath);
 		$this->_exec('ls -la ' . $this->cmsPath);
 
 		$this->say('Joomla CMS site created at ' . $this->cmsPath);
